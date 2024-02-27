0bc58bb499de5bdc623d56de4a4b1ccd5a7a8694
jamiemcgraner <110130113+jdanielle12@users.noreply.github.com>
2024-02-26 19:45:42 -0700

    created an instand of the account class and calculated interest earned

--- savings_account.py
+++ savings_account.py
@@ -1,12 +1,9 @@
 from Account import Account
 
 def create_savings_account(balance, interest_rate, months):
-    # Create an instance of the `Account` class and pass in the balance and interest parameters.
-    #  Hint: You need to add the interest as a value, i.e, 0.
-    # ADD YOUR CODE HERE
+    savings_account = Account(balance, interest_rate)
 
-    # Calculate interest earned
-    # ADD YOUR CODE HERE
+    interest_earned = savings_account.get_balance() * (interest_rate / 100)
 
     # Update the savings account balance by adding the interest earned
     # ADD YOUR CODE HERE
