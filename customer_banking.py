c66a017143cdc93afa35e0c4b78b2f87a4c79623
jamiemcgraner <110130113+jdanielle12@users.noreply.github.com>
2024-02-26 20:03:24 -0700

    deleting comments

--- customer_banking.py
+++ customer_banking.py
@@ -1,12 +1,7 @@
 from savings_account import create_savings_account
 from cd_account import create_cd_account
 
-# Define the main function
 def main():
-    """This function prompts the user to enter the savings and cd account balance, interest rate,
-    and the length of months to determine the interest gained.
-    It displays the interest earned on the savings and CD accounts and updates the balances.
-    """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
 
