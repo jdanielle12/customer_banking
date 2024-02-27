34776ededde8b6e38a8708d97bca3422ab85531d
jamiemcgraner <110130113+jdanielle12@users.noreply.github.com>
2024-02-26 19:50:46 -0700

    removing comments

--- cd_account.py
+++ cd_account.py
@@ -1,18 +1,6 @@
 from Account import Account
-# ADD YOUR CODE HERE
 
 def create_cd_account(balance, interest_rate, months):
-    """Creates a CD account, calculates interest earned, and updates the account balance.
-
-    Args:
-        balance (float): The initial CD account balance.
-        interest_rate (float): The APR interest rate for the CD account.
-        months (int): The length of months for the CD.
-
-    Returns:
-        float: The updated CD account balance after adding the interest earned.
-        And returns the interest earned.
-    """
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
